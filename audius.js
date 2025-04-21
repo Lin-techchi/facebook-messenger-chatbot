45ca5edaa94467042b2aa015a7ca8e01076c9071
Lin-techchi <mustafizurr440@gmail.com>
2025-04-21 08:59:07 +0600

    Switched to Audius API

--- audius.js
+++ audius.js
@@ -1,35 +1,35 @@
 const axios = require('axios');
 
-// Define Audius API endpoint
 const AUDIUS_API_URL = 'https://discoveryprovider.audius.io/v1/tracks/search';
 
 // Function to search tracks from Audius
 async function searchAudiusTrack(query) {
-    try {
-        const response = await axios.get(AUDIUS_API_URL, {
-            params: {
-                query,   // The song name you want to search for
-                limit: 1  // Limit results to 1 track
-            }
-        });
+  try {
+    const response = await axios.get(AUDIUS_API_URL, {
+      params: {
+        query,   // The song name you want to search for
+        limit: 1  // Limit results to 1 track
+      }
+    });
 
-        const tracks = response.data.data;
+    console.log('Audius API Response:', response.data);  // Log the entire response for debugging
 
-        // If we find a track, return relevant data
-        if (tracks.length > 0) {
-            const track = tracks[0];  // Get the first track result
-            return {
-                title: track.title,
-                artist: track.user.name,
-                streamUrl: track.permalink  // URL to the full track
-            };
-        } else {
-            return null;  // No track found
-        }
-    } catch (err) {
-        console.error('Error fetching from Audius:', err);
-        throw new Error('Error fetching track from Audius API');  // Propagate error with custom message
+    const tracks = response.data.data;
+
+    if (tracks.length > 0) {
+      const track = tracks[0];
+      return {
+        title: track.title,
+        artist: track.user.name,
+        streamUrl: track.permalink
+      };
+    } else {
+      return null;
     }
+  } catch (err) {
+    console.error('Error fetching from Audius:', err);
+    throw new Error('Error fetching track from Audius API');
+  }
 }
 
 module.exports = { searchAudiusTrack };
